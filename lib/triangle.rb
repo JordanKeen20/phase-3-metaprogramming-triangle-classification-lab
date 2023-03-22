df7f1565e89646ea928b1e0f32e75ff572d9ab28
jordanlynne9@gmail.com <jordanlynne9@gmail.com>
2023-03-21 20:12:31 -0700

    completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
   # write code here
+  attr_accessor :tri1, :tri2, :tri3
+
+  def initialize(tri1, tri2, tri3)
+    @tri1= tri1
+    @tri2= tri2
+    @tri3= tri3
+  end
+
+  def kind
+    validate_triangle
+    if tri1 == tri2 && tri2 == tri3
+      :equilateral
+    elsif tri1 == tri2 || tri2 == tri3 || tri1 == tri3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero?
+    [tri1, tri2, tri3].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    tri1 + tri2> tri3 && tri1 + tri3> tri2 && tri2 + tri3 > tri1
+  end
+
+  
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+    
+  end
+
 end
